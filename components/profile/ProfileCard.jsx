d53a67e11813b11de89e256f2fd06b6318873c29
yafet-123 <yafetaddisu123@gmail.com>
2023-08-02 05:19:37 -0700

    changing the image at profile

--- components/profile/ProfileCard.jsx
+++ components/profile/ProfileCard.jsx
@@ -5,14 +5,14 @@ export default function ProfileCard() {
   return (
     <div className="flex flex-col justify-center items-center lg:pb-20">
         <Image
-          src="https://res.cloudinary.com/df7hlpjcj/image/upload/v1687270557/Portfolio/1678034902830_cniqik.png"
+          src="https://res.cloudinary.com/df7hlpjcj/image/upload/v1690978117/Ecommerce/photo-1438761681033-6461ffad8d80_qvkvak.jpg"
           priority
           width={200}
           height={200}
           className="h-[200px] bg-cover rounded-full"
         />
         <p className="font-poppins text-xl lg:text-3xl font-normal text-center text-[#505e66] my-5 w-full">
-          Name
+          Yafet Addisu
         </p>
     </div>
   );
