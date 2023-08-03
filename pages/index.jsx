44a319903ad5a6db5d022b0e4e46cc3b7f12b524
yafet-123 <yafetaddisu123@gmail.com>
2023-08-03 03:27:53 -0700

    adding the categories field

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { MainHeader } from '../components/common/MainHeader';
 import List from '../components/Home/List';
-
+import ListCategories from "../components/Categories/List";
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full py-0 pt-32">
       <MainHeader title="Hulu Media Ecommerce : Home" />
-      <List />
+      <div className="flex flex-col lg:flex-row px-2 lg:px-36">
+        <ListCategories />
+        <List />
+      </div>
     </div>
   )
 }
