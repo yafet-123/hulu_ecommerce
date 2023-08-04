a5f500806b5ac67d92ac59666c4d3ac9f2f3a997
yafet-123 <yafetaddisu123@gmail.com>
2023-08-04 05:25:11 -0700

    editing the categories list

--- components/Categories/Display.jsx
+++ components/Categories/Display.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Display({ categorie }) {
   return (
     <Link href="/ " className="flex flex-col justify-between lg:pb-10">
-      <h1>{categorie.name}</h1>
+      <p className="flex font-poppins text-md lg:text-lg font-normal text-left text-[#505e66] my-3">{categorie.name}</p>
     </Link>
   );
 }
