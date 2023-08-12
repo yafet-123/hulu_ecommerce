0349a021cf7745901042d9bbe2d221cd4c2284a2
yafet-123 <yafetaddisu123@gmail.com>
2023-08-11 19:01:22 -1200

    editing the navbar signin and login button

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,10 +1,10 @@
 import {LoginForm} from "../components/Login/Form";
 import { MainHeader } from '../components/common/MainHeader';
 
-export default function CreateUser() {
+export default function Login() {
   return (
     <main className="w-full h-full px-2 lg:px-10 py-32 bg-white flex flex-col">
-      <MainHeader title="Hulu Media Ecommerce : CreateUser" />
+      <MainHeader title="Hulu Media Ecommerce : Login" />
       <LoginForm />
     </main> 
   )
