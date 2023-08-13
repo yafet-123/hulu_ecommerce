c74dc446eac174968d85ba8c2dc9a2190a1c12d9
yafet-123 <yafetaddisu123@gmail.com>
2023-08-12 20:07:31 -1200

    adding the api in the items

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,40 @@ import Image from 'next/image'
 import { MainHeader } from '../components/common/MainHeader';
 import List from '../components/Home/List';
 import ListCategories from "../components/Categories/List";
-export default function Home() {
+
+export async function getStaticProps(){
+  const items = await prisma.Items.findMany({
+    orderBy : {
+      CreatedDate:'desc'
+    },
+    include:{
+      User:{
+        select:{
+          UserName:true
+        }
+      },
+    }
+  });
+
+  const AllItems = items.map((data)=>({
+    items_id:data.items_id,
+    name:data.name,
+    Description:data.Description,
+    price:data.price,
+    Image:data.Image,
+    profile:data.profile,
+    Condition:data.Condition,
+    CreatedDate:data.CreatedDate
+  })
+
+  return{
+    props:{
+      AllItems:JSON.parse(JSON.stringify(AllItems)),
+    }
+  }
+}
+
+export default function Home({AllItems}) {
   return (
     <div className="flex flex-col w-full h-full py-0 pt-32">
       <MainHeader title="Hulu Media Ecommerce : Home" />
