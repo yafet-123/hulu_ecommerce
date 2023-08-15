9c985ab2768f0acbdfb73e8baafd838d2dedfb59
yafet-123 <yafetaddisu123@gmail.com>
2023-08-14 17:12:54 -1200

    adding the Admin user

--- components/Admin/User/DisplayUser.js
+++ components/Admin/User/DisplayUser.js
@@ -1,158 +1,53 @@
 import React from "react";
 import { useState,useEffect, useContext} from 'react'
 import moment from 'moment';
+import Image from "next/image";
 import { useRouter } from 'next/router'
-import {DeleteUser} from './DeleteUser.js'
-import {UpdateUser} from './UpdateUser.js'
+// import {DeleteUser} from './DeleteUser.js'
+// import {UpdateUser} from './UpdateUser.js'
 
-export function DisplayUser({users}) {
+export function DisplayUser({user}) {
     const router = useRouter();
-    const [deletemodalOn, setdeleteModalOn] = useState(false);
-    const [updatemodalOn, setupdateModalOn] = useState(false);
-    const [deleteuserid,setdeleteuserid] = useState()
-    const [updateuserid,setupdateuserid] = useState()
-    const [updateemail, setupdateemail] = useState("")
-    const [updateusername,setupdateusername] = useState("")
+    const [copied, setCopied] = useState("");
 
-    const clickedFordelete = () => {
-        setdeleteModalOn(true)
-    }
-
-    const clickedForupdate = () => {
-        setupdateModalOn(true)
-    }
+    const handleCopy = () => {
+        setCopied(user.email);
+        navigator.clipboard.writeText(user.email);
+        setTimeout(() => setCopied(false), 3000);
+    };
 
     return (
         <div className="px-0 lg:px-10">
-            <div className="p-2 lg:p-5">
-                <div className="overflow-auto rounded-lg shadow hidden md:block">
-                    <table className="w-full">
-                        <thead className="bg-neutral-100 dark:bg-slate-800 border-b-2 border-gray-200">
-                            <tr>
-                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Id</th>
-                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">User Name</th>
-                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Email</th>
-                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Created Date</th>
-                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Modified Date</th>
-                            </tr>
-                        </thead>
-                        <tbody className="divide-y divide-gray-100">
-                            {users.map((data,index)=>(
-                                <tr key={index} className="even:bg-neutral-300 odd:bg-neutral-200 even:dark:bg-gray-900 odd:dark:bg-gray-800 w-full">
-                                    <td className="p-3 text-lg text-gray-700 whitespace-nowrap">
-                                        <p className="font-bold text-[#009688] dark:text-white hover:underline">{data.user_id}</p>
-                                    </td>
-                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
-                                        {data.UserName}
-                                    </td>
-
-                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
-                                        <h1 className="text-black dark:text-white flex justify-between my-5 font-bold text-lg md:text-xl">
-                                            <span className={ `font-normal font-medium ${data.email ? " " : "text-red-800"}`}>
-                                                { data.email ? data.email : "No Email Address" }
-                                            </span>
-                                        </h1>
-                                    </td>
-
-                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
-                                        {moment(data.createDate).utc().format('YYYY-MM-DD')}
-                                    </td>
-                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
-                                        {moment(data.ModifiedDate).utc().format('YYYY-MM-DD')}
-                                    </td>
-
-                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
-                                        <button
-                                            onClick={() => {
-                                                clickedForupdate()
-                                                setupdateuserid(data.user_id)
-                                                setupdateusername(data.UserName)
-                                                setupdateemail(data.email)
-                                            }} 
-                                            className="bg-[#009688] text-white font-bold py-2 px-4 border-b-4 border-[#009688] hover:scale-110 duration-1000 ease-in-out rounded">
-                                            Edit
-                                        </button>
-                                    </td>
-
-                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
-                                        <button 
-                                            onClick={() => {
-                                                clickedFordelete()
-                                                setdeleteuserid(data.user_id)
-                                            }}
-                                            className="bg-red-500 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:scale-110 duration-1000 ease-in-out rounded"
-                                        >
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+           <div className="flex justify-between items-start gap-5">
+                <div className="flex-1 flex justify-between items-center gap-3 cursor-pointer">
+                  <h3 className="font-satoshi font-semibold text-gray-900">
+                    {user.user_id}
+                  </h3>
+                  <p className="font-inter text-sm text-gray-500">
+                    {moment(user.ModifiedDate).utc().format("YYYY-MM-DD")}
+                  </p>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 md:hidden">
-                    {users.map((data,index)=>(
-                        <div key={index} className=" bg-neutral-200 dark:bg-slate-800 space-y-3 p-2 lg:p-4 rounded-lg shadow overflow-scroll">
-                            <div>
-                                <p className="text-blue-500 dark:text-white font-bold hover:underline">
-                                    <span className="text-lg">Id : </span> 
-                                    <span className="text-sm ">{data.user_id}</span>
-                                </p>
-                            </div>
-                            <div className="text-gray-700 dark:text-white font-bold">
-                                <span className="text-lg">User Name : </span>
-                                <span className="text-md">{data.UserName} </span>
-                            </div>
 
-                            <div className="text-md lg:text-lg text-gray-700 dark:text-white font-bold break-words ">
-                                Email : <span className={ `font-normal font-medium ${data.email ? " " : "text-red-800"}`}>
-                                    { data.email ? data.email : "No Email Address" }
-                                </span>
-                            </div>
-
-                            <div className="text-black font-bold dark:text-white">
-                              <span className="text-lg">createDate : </span>
-                              <span className="text-sm">{moment(data.createDate).utc().format('YYYY-MM-DD')}</span>
-                            </div>
-                            <div className="text-black font-bold dark:text-white">
-                              <span className="text-lg">Modified Date : </span>
-                              <span className="text-sm">{moment(data.ModifiedDate).utc().format('YYYY-MM-DD')}</span>
-                            </div>
-
-                            <div className="flex items-center justify-between text-sm">
-                                <button
-                                    onClick={() => {
-                                        clickedForupdate()
-                                        setupdateuserid(data.user_id)
-                                        setupdateusername(data.UserName)
-                                        setupdateemail(data.email)
-                                    }}  
-                                    className="bg-[#009688] text-white font-bold py-2 px-4 border-b-4 border-[#009688] hover:scale-110 duration-1000 ease-in-out rounded">
-                                    Edit
-                                </button>
-
-                                <button
-                                    onClick={() => {
-                                        clickedFordelete()
-                                        setdeleteuserid(data.user_id)
-                                    }} 
-                                    className="bg-red-500 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:scale-110 duration-1000 ease-in-out rounded"
-                                >
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-                    ))}
+                <div className="copy_btn" onClick={handleCopy}>
+                  <Image
+                    src={
+                      copied === user.email
+                        ? "/assets/icons/tick.svg"
+                        : "/assets/icons/copy.svg"
+                    }
+                    alt={copied === user.email ? "tick_icon" : "copy_icon"}
+                    width={12}
+                    height={12}
+                  />
                 </div>
             </div>
 
-            {deletemodalOn && 
-                <DeleteUser setdeleteModalOn={setdeleteModalOn} deleteuserid={deleteuserid}/>
-            }
-
-            {updatemodalOn && 
-                <UpdateUser setupdateModalOn={setupdateModalOn} updateuserid={updateuserid} updateemail={updateemail} updateusername={updateusername} setupdateemail={setupdateemail} setupdateusername={setupdateusername} />
-            }
+            <div className="flex items-center justify-between">
+                <p className="my-4 font-satoshi text-sm text-gray-700">
+                    {user.UserName}
+                </p>
+                <p className="my-4 font-satoshi text-sm text-gray-700">{user.email}</p>
+            </div>
         </div>
     );
 }
--- pages/Admin/User/index.js
+++ pages/Admin/User/index.js
@@ -1,8 +1,38 @@
 import { VerticalNavbar } from "../../../components/Admin/VerticalNavbar";
 import { MainHeader } from '../../../components/common/MainHeader';
+import { DisplayUser } from '../../../components/Admin/User/DisplayUser'; 
+import { prisma } from '../../../util/db.server.js'
 
-export default function User(){
-  
+export async function getStaticProps(){
+  const users = await prisma.User.findMany({orderBy : {ModifiedDate:'desc'}});
+  const Allusers = users.map((data)=>({
+      user_id:data.user_id,
+      UserName:data.UserName,
+      phonenumber:data.phonenumber,
+      email:data.email,
+      password:data.password,
+      CreatedDate:data.CreatedDate,
+      ModifiedDate:data.ModifiedDate
+  }))
+
+  return{
+    props:{
+      Allusers:JSON.parse(JSON.stringify(Allusers))
+    }
+  }
+}
+
+const UserCardList = ({ Allusers }) => {
+  return (
+    <div className="mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {Allusers.map((user) => (
+        <DisplayUser key={user.user_id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+export default function User({Allusers}){
   const data = {
     "name":"yafet"
   }
@@ -14,7 +44,7 @@ export default function User(){
       <MainHeader title="Hulu Media Ecommerce : Admin" />
       <div className="flex pt-10">
         <VerticalNavbar onChange={handleChange} data={data} />
-        
+        <UserCardList Allusers={Allusers} />
       </div>
     </main>
   );
