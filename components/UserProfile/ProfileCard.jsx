91258e734cd1165f7e5e63950a0bd7704c9fab50
yafet-123 <yafetaddisu123@gmail.com>
2023-08-29 14:19:17 -1200

    editing the next.config file

--- components/UserProfile/ProfileCard.jsx
+++ components/UserProfile/ProfileCard.jsx
@@ -7,9 +7,9 @@ export default function ProfileCard({user}) {
         <Image
           src={user.Image}
           priority
-          width={100}
-          height={100}
-          className="h-[100px] bg-cover rounded-full"
+          width={200}
+          height={200}
+          className="h-[200px] bg-cover rounded-full"
           alt="profile image"
         />
         <p className="font-poppins text-xl lg:text-3xl font-normal text-center text-[#505e66] mt-5 w-full">
