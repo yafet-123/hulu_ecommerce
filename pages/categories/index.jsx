5428f8958ed2b749fc2c5fe31199101081c56ea5
yafet-123 <yafetaddisu123@gmail.com>
2023-08-31 08:03:59 -1200

    editing the login form page

--- pages/categories/index.jsx
+++ pages/categories/index.jsx
@@ -6,7 +6,7 @@ import ListCategories from "../../components/Categories/List";
 import { prisma } from '../../util/db.server.js'
 import { useSession } from "next-auth/react";
 
-export async function getStaticProps(context){
+export async function getServerSideProps(context){
   const {params,req,res,query} = context
   const name = query.name
   const items = await prisma.Items.findMany({
@@ -60,7 +60,7 @@ export async function getStaticProps(context){
   }
 }
 
-export default function Home({AllItems, categories}) {
+export default function Categories({AllItems, categories}) {
   const { status, data: session } = useSession();
   return (
     <div className="flex flex-col w-full h-full py-0 pt-32">
