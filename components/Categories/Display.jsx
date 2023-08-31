eb06fd9c4f66546373fde8c25b2569d4aa0e879a
yafet-123 <yafetaddisu123@gmail.com>
2023-08-31 07:04:09 -1200

    editing the login form page

--- components/Categories/Display.jsx
+++ components/Categories/Display.jsx
@@ -4,7 +4,13 @@ import { useState } from "react";
 
 export default function Display({ categorie }) {
   return (
-    <Link href="/ " className="flex flex-col justify-between pb-5">
+    <Link 
+      href={{
+        pathname: '/categorie',
+        query: { name: categorie.CategoryName },
+      }} 
+      className="flex flex-col justify-between pb-5"
+    >
       <p className="flex font-poppins text-md lg:text-lg font-normal text-left text-[#505e66] lg:pl-5">{categorie.CategoryName}</p>
     </Link>
   );
---
+++ pages/categories/index.jsx
@@ -0,0 +1,74 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { MainHeader } from '../../components/common/MainHeader';
+import List from '../../components/Home/List';
+import ListCategories from "../../components/Categories/List";
+import { prisma } from '../util/db.server.js'
+import { useSession } from "next-auth/react";
+
+export async function getStaticProps(context){
+  const {params,req,res,query} = context
+  const name = query.name
+  const items = await prisma.Items.findMany({
+    where:{
+      CategoryName: name,
+    },
+    orderBy : {
+      CreatedDate:'desc'
+    },
+    include:{
+      User:{
+        select:{
+          UserName:true,
+          Image:true,
+          user_id:true
+        }
+      },
+    }
+  });
+
+  const categories = await prisma.Category.findMany({
+    orderBy : {
+      CreatedDate:'desc'
+    },
+    include:{
+      User:{
+        select:{
+          UserName:true
+        }
+      },
+    }
+  });
+
+  const AllItems = items.map((data)=>({
+    items_id:data.items_id,
+    name:data.name,
+    Description:data.Description,
+    price:data.price,
+    Image:data.Image,
+    profile:data.profile,
+    user:data.User,
+    Condition:data.Condition,
+    CreatedDate:data.CreatedDate
+  }))
+
+  return{
+    props:{
+      AllItems:JSON.parse(JSON.stringify(AllItems)),
+      categories:JSON.parse(JSON.stringify(categories))
+    }
+  }
+}
+
+export default function Home({AllItems, categories}) {
+  const { status, data: session } = useSession();
+  return (
+    <div className="flex flex-col w-full h-full py-0 pt-32">
+      <MainHeader title="Hulu Media Ecommerce : Home" />
+      <div className="flex flex-col-reverse lg:flex-row px-2 lg:px-36">
+        <ListCategories categories={categories} />
+        <List AllItems={AllItems} session={session} />
+      </div>
+    </div>
+  )
+}
