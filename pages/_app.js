61cd57a3994971a95fac904853e6b144e4fb3d4b
yafet-123 <yafetaddisu123@gmail.com>
2023-09-04 14:38:08 -1200

    editing the theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,13 @@ import '../styles/globals.css'
 import { Navbar } from '../components/common/Navbar';
 import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from 'next-themes';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
+import ReactDOM from "react-dom";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return(
