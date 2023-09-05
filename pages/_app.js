74db1cafaecbf3d1723f2543d30b12a9425a4e87
yafet-123 <yafetaddisu123@gmail.com>
2023-09-04 13:38:57 -1200

    editing the theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import { Navbar } from '../components/common/Navbar';
 import { SessionProvider } from "next-auth/react"
+import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return(
     <SessionProvider session={session}>
-      <div>
-        <Navbar />
-        <Component {...pageProps} />
-      </div>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <div>
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
     </SessionProvider>
   )
 }
